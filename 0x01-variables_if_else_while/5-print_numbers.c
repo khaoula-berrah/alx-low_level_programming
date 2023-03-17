56c7efc2a8a065d0dd46199de7ccdf660b7325c1
khaoula-berrah <berah.khaoula@gmail.com>
2023-03-17 12:39:30 -0700

    create numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Explain: 'create numbers'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		printf("%d", c);
+	}
+
+	putchar('\n');
+	return (0);
+}
