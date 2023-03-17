f225946017860af09534fcfd58427442a8d88c89
khaoula-berrah <berah.khaoula@gmail.com>
2023-03-17 14:08:49 -0700

    patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Explain: 'Patience persistence'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int f;
+
+	for (f = 48; f <= 57; f++)
+	{
+		putchar(f + 44 + 32);
+	}
+	putchar('\n');
+	return (0);
+}
