446a6c843d169ae28d692f3052f04567fedf6dff
khaoula-berrah <berah.khaoula@gmail.com>
2023-03-17 07:18:05 -0700

    create pos neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,8 +13,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n >= 0)
-		printf("%d is possitive or equal zero\n", n);
+		printf("%d is positive or equal zero", n);
 	else
-		printf("%d is negative\n", n);
+		printf("%d is negative", n);
+	printf("\n")
 	return (0);
 }
