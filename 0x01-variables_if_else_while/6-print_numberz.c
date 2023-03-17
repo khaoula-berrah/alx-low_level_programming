2988cf5800bf16c3ecc743ec8fa05c86291d444e
khaoula-berrah <berah.khaoula@gmail.com>
2023-03-17 12:51:37 -0700

    create numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Explain: ''
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	for (r == 0; r <= 9; r++)
+		putchar(r + 48);
+	putchar('\n');
+	return (0);
+}
