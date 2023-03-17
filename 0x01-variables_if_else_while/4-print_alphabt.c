892df9042839eea249bd23467aa53cbda38718ae
khaoula-berrah <berah.khaoula@gmail.com>
2023-03-17 11:50:55 -0700

    create alphabets soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,6 +3,24 @@
 #include <stdio.h>
 /**
  *main - Entry point
- *Explain: ''
+ *Explain: 'prints the alphabet in lowercase'
  *Return: Always 0 (Success)
  */
+int main(void)
+{
+	int s = 97;
+
+	while (s <= 122)
+	{
+		if (s == 101 || s == 113)
+		{
+			s++;
+			continue;
+		}
+		putchar(s);
+		s++;
+	}
+	putchar('\n');
+
+	return (0);
+}
