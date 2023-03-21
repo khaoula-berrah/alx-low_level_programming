7e1ae851e554441e4954a3a0824ac8b052b32a80
khaoula-berrah <berah.khaoula@gmail.com>
2023-03-21 04:35:11 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *Explain: ''
+ *Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
