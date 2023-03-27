31ee219f8183e778d1714f2fd0bfb8fd081be5b7
khaoula-berrah <berah.khaoula@gmail.com>
2023-03-27 09:53:30 -0700

    7_puts

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,22 +11,20 @@
 
 void puts_half(char *str)
 {
-	int i;
-	int len = strlen(str);
-	int start;
+	int a, n, longi;
 
-	if (len % 2 == 0)
-	{
-		start = len / 2;
-	}
-	else
-	{
-		start = (len - 1) / 2;
-	}
+	longi = 0;
 
-	for (i = start; i < len; i++)
-	{
-		_putchar(str[i]);
-	}
+	for (a = 0; str[a] != '\0'; a++)
+		longi++;
+
+	n = (longi / 2);
+
+	if ((longi % 2) == 1)
+		n = ((longi + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
 	_putchar('\n');
+
 }
