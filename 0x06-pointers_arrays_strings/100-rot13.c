f663565126027747b90146630b6da30a4c49fc2e
khaoula-berrah <berah.khaoula@gmail.com>
2023-03-29 12:58:46 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	char *p = s;
+	while (*p)
+	{
+		if (isalpha(*p))
+		{
+			char base = isupper(*p) ? 'A' : 'a';
+			*p = (((*p - base) + 13) % 26) + base;
+		}
+		p++;
+	}
+	return (s);
+}
