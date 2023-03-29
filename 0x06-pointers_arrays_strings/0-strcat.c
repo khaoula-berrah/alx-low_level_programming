f7ade8eb363fc24c1993df6e9d4b3fa12d3abdab
khaoula-berrah <berah.khaoula@gmail.com>
2023-03-29 10:12:05 -0700

    more pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,14 +8,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int dest_len = strlen(dest);
+	int dest = strlen(dest);
 	int i;
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[dest_len + i] = src[i];
+		dest[dest + i] = src[i];
 	}
-	dest[dest_len + i] = '\0';
+	dest[dest + i] = '\0';
 
 	return (dest);
 }
