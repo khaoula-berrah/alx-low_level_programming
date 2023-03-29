5bbaeaf079cb94ba8d54f84060ce7e19337f7f07
khaoula-berrah <berah.khaoula@gmail.com>
2023-03-29 12:38:39 -0700

    more pointers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,22 +4,22 @@
  * @n: input value
  * Return: n value
  */
-char *leet(char *str)
+char *leet(char *n)
 {
 	int i, j;
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (str[i] == s1[j])
+			if (n[i] == s1[j])
 			{
-				str[i] = s2[j];
+				n[i] = s2[j];
 			}
 		}
 	}
-	return (str);
+	return (n);
 }
 
