42672ccbdb95a3c971b3a97898232ccdbbd77cc9
khaoula-berrah <berah.khaoula@gmail.com>
2023-04-13 17:15:58 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,30 +3,30 @@
 #include <stdio.h>
 #define ERR_MSG "Error"
 /**
-  * multiply - checks if a string contains a non-digit char
-  * @s: string to be evaluated
-  * Return: 0 if a non-digit is found, 1 otherwise
-  */
-int multiply(char *s)
+ * digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int digit(char *s)
 {
-	int m = 0;
+	int x = 0;
 
-	while (s[m])
+	while (s[x])
 	{
-		if (s[m] < '0' || s[m] > '9')
+		if (s[x] < '0' || s[x] > '9')
 			return (0);
-		m++;
+		x++;
 	}
 	return (1);
 }
 /**
-  * _strlen - returns the length of a string
-  * @s: string to evaluate
-  * Return: the length of the string
-  */
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: the length of the string
+ */
 int _strlen(char *s)
 {
-	int i;
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
@@ -35,30 +35,29 @@ int _strlen(char *s)
 	return (i);
 }
 /**
-  * error - handles errors for main
-  */
-void error(void)
+ * errors - handles errors for main
+ */
+void errors(void)
 {
 	printf("Error\n");
 	exit(98);
 }
 /**
-  * main - multiplies two positive numbers
-  * @argc: number of arguments
-  * @argv: array of arguments
-  * Return: always 0 (Success)
-  */
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-	char *num1, *num2;
-	int ll1, ll2, ll, i, *reslt, crry, dd1, dd2, j = 0;
+	char *s1, *s2;
+	int ll1, ll2, ll, i, carry, dd1, dd2, *reslt, j = 0;
 
-	num1 = argv[1], num2 = argv[2];
-	if (argc != 3 || !multiply(num1) || !multiply(num2))
-		error();
-
-	ll1 = _strlen(num1);
-	ll2 = _strlen(num2);
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !digit(s1) || !digit(s2))
+		errors();
+	ll1 = _strlen(s1);
+	ll2 = _strlen(s2);
 	ll = ll1 + ll2 + 1;
 	reslt = malloc(sizeof(int) * ll);
 	if (!reslt)
@@ -67,17 +66,17 @@ int main(int argc, char *argv[])
 		reslt[i] = 0;
 	for (ll1 = ll1 - 1; ll1 >= 0; ll1--)
 	{
-		dd1 = num1[ll1] - '0';
-		crry = 0;
-		for (ll2 = _strlen(num2) - 1; ll2 >= 0; ll2--)
+		dd1 = s1[ll1] - '0';
+		carry = 0;
+		for (ll2 = _strlen(s2) - 1; ll2 >= 0; ll2--)
 		{
-			dd2 = num2[ll2] - '0';
-			crry += reslt[ll1 + ll2 + 1] + (dd1 * dd2);
-			reslt[ll1 + ll2 + 1] = crry % 10;
-			crry /= 10;
+			dd2 = s2[ll2] - '0';
+			carry += reslt[ll1 + ll2 + 1] + (dd1 * dd2);
+			reslt[ll1 + ll2 + 1] = carry % 10;
+			carry /= 10;
 		}
-		if (crry > 0)
-			reslt[ll1 + ll2 + 1] += crry;
+		if (carry > 0)
+			reslt[ll1 + ll2 + 1] += carry;
 	}
 	for (i = 0; i < ll - 1; i++)
 	{
