f3dbcc4d5e5aab327c80610b02e7d0c83bd9f64a
khaoula-berrah <berah.khaoula@gmail.com>
2023-04-27 11:44:56 -0700

    task 7

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,9 @@
 global main
-
-section .data
-	hello db "Hello, Holberton", 0
-	nl db 10, 0
-
-section .text
-	global main
-
+	extern printf
 main:
-	push hello
+	mov   edi, hello
+	xor   eax, eax
 	call printf
-	add rsp, 8  ; clean up the stack
-	push nl
-	call printf
-	add rsp, 8  ; clean up the stack
-
-	mov eax, 0  ; return 0
+	mov eax, 0
 	ret
+hello: db `Hello, Holberton\n`,0
